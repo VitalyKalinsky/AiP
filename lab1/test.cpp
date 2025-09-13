bcda910b6f5438df910e8ee3356d50f29c0ff40e
VitalyKalinsky <kalinskyvii@gmail.com>
2025-09-13 15:23:22 +0300

    lab1 task4

--- lab1/test.cpp
+++ lab1/test.cpp
@@ -1,9 +1,26 @@
-#include <stdio.h>
+#include <iostream>
 
 int main() {
-    char c1 = 127, c2 = 2;
-    char res = c1 + c2;
-    printf("%d",(int) res);
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Outer loop iteration: " << i << std::endl;
+        int choice = i % 2; // Simulate a condition for the switch
 
+        switch (choice) {
+            case 0:
+                std::cout << "  Inside switch, case 0." << std::endl;
+                // If 'continue' is here, it will skip the remaining part of the 'for' loop for this 'i'
+                if (i == 2) {
+                    continue; // This 'continue' affects the 'for' loop
+                }
+                break;
+            case 1:
+                std::cout << "  Inside switch, case 1." << std::endl;
+                break;
+            default:
+                std::cout << "  Inside switch, default." << std::endl;
+                break;
+        }
+        std::cout << "After switch, in outer loop." << std::endl;
+    }
     return 0;
 }
