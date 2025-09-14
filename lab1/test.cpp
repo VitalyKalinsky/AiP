0ffe8394ba8c95a55abb8183e8498ade2a7d0c2a
VitalyKalinsky <kalinskyvii@gmail.com>
2025-09-15 01:14:53 +0300

    lab1 done

--- lab1/test.cpp
+++ lab1/test.cpp
@@ -1,16 +1,26 @@
-namespace my
-{ int x ; int y ; } ;
-namespace your
-{ int x ; int y ; } ;
+#include <iostream>
+
 int main(){
-    int x;
-    x = 10; // ???
-    using  my::x ;
-    x = 11; // ???
-    y = 11; // ???
-    my :: x = 12; // ??
-    using namespace my ;
-    using namespace your ;
-    x = 20;
-    y = 20;
+/**
+ * Задание 10.3. 
+ *
+ * Напишите фрагмент кода, который реализует с помощью while следующую логику: 
+ *
+ * 1. исходно int x = 0; 
+ * 2. на каждой итерации x = x + 1, sum = sum + 1 / x;
+ * 3. необходимо найти значение x, при котором sum > 1.7.
+ */
+
+{
+// char start = 120, sum = 0;
+// for (char n = start; n < start + 10; ++n) {
+//    sum += n;
+// }
+// в char не поместятся значения больше 127, поэтому я изменил тип start и sum на short
+short start = 120, sum = 0;
+for (short n = start; n < start + 10; ++n) {
+   sum += n;
+}
+std::cout << sum;
+}
 }
